ffd8bedfa37b481539b968b2c34dd37f61f900e5
Daniel <drones9182@gmail.com>
2021-05-24 19:24:09 -0500

    Estandar de JSON compatible con USB

--- LectorUSB.py
+++ LectorUSB.py
@@ -2,12 +2,20 @@ import json
 class LectorUSB():
     def __init__(self, archivoUSB):
         self.json = json
-        self.archivoUSB = self.json.load(archivoUSB)
+        self.archivoUSB = archivoUSB
+        self.resultadoUSB = {"type":None, "action":None, "repeat":None, "time":None}
     def buscarInformacion(self):
-        info = []
         for clave in self.archivoUSB: 
-            for valor in self.archivoUSB[clave]:
-                print(self.archivoUSB[clave][valor])
-                info.append((valor, self.archivoUSB[clave][valor]))
-        return info
-        
+            with open(clave) as json_file:
+                auxiliarJSON = self.json.load(json_file)
+                type = auxiliarJSON["usb"]["type"]
+                function = auxiliarJSON["usb"]["funcion"]
+                if type == 0: 
+                    self.resultadoUSB["type"] = function
+                elif type == 1:
+                    self.resultadoUSB["action"] = function
+                elif type == 2:
+                    self.resultadoUSB["repeat"] = function
+                elif type == 3:
+                    self.resultadoUSB["time"] = function
+        return self.resultadoUSB
